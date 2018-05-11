2713a7b3ab44f9efef0c68ac8aaeae491c222319
tim <talutova.9888@gmail.com>
2018-05-11 14:26:59 +0400

    addMoney class

---
+++ TDD.Wallet/Wallet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Wallet
+{
+    public class Wallet
+    {
+      
+        public void addMoney(string course, int amount)
+        {
+            if (wallet[course] == null)
+            {
+                wallet.Add(course, amount);
+            }
+            else
+            {
+                wallet[course] = (int)wallet[course] + amount;
+            }
+
+            printer.print("Add", course, amount.ToString());
+        }
+    }
+}
