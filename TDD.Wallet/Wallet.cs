8261dbb2c5c61420c57a2c676b9188a10bde717d
tim <talutova.9888@gmail.com>
2018-05-11 14:43:18 +0400

    Money printer + class

--- TDD.Wallet/Wallet.cs
+++ TDD.Wallet/Wallet.cs
@@ -12,11 +12,20 @@ namespace TDD.Wallet
         public Hashtable wallet = new Hashtable();
         Bank bank;
         public MoneyPrinter printer;
+        private Bank b;
+        private MoneyPrinter_plus mp;
 
         public Wallet(Bank bank1, MoneyPrinter printer1)
         {
             bank = bank1;
             printer = printer1;
+        }
+
+        public Wallet(Bank b, MoneyPrinter_plus mp)
+        {
+            // TODO: Complete member initialization
+            this.b = b;
+            this.mp = mp;
         }
 
         public void addMoney(string course, int amount)
@@ -31,6 +40,33 @@ namespace TDD.Wallet
             }
 
             printer.print("Add", course, amount.ToString());
+        }
+
+        int remov(string course, int amount)
+        {
+            if ((int)wallet[course] < amount)
+            {
+                throw new System.OverflowException();
+            }
+            else
+            {
+                return (int)wallet[course] - amount;
+            }
+        }
+
+        public void removeMoney(string course, int amount)
+        {
+            try
+            {
+                wallet[course] = remov(course, amount);
+
+                if ((int)wallet[course] == 0)
+                    wallet.Remove(course);
+            }
+            catch (OverflowException e)
+            {
+            }
+            printer.print("Remove", course, amount.ToString());
         }
     }
 }
